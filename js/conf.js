0902349abdb3d5b33782555359e94626c754008a
Hoover H.F. Yin <hooveryin@live.hk>
2019-11-01 04:04:51 +0800

    Test JSON

--- js/conf.js
+++ js/conf.js
@@ -1,18 +1,19 @@
 $.getJSON("js/conf.json", function(json) {
-	$('#conf').html('<ul class="list-group" id="list">');
+	var code = '<ul class="list-group" id="list">';
 	for(cite in json){
 		var item = eval("json."+cite);
-		$('#conf').append('<li class="list-group-item">'+item.author+', "');
+		code += '<li class="list-group-item">'+item.author+', "';
 		if(item.url)
-			$('#conf').append('<a href="'+item.url+'">');
-		$('#conf').append(item.title);
+			code += '<a href="'+item.url+'">';
+		code += item.title;
 		if(item.url)
-			$('#conf').append('</a>');
-		$('#conf').append('," <i>'+item.booktitle+'</i>, ');
+			code += '</a>';
+		code += '," <i>'+item.booktitle+'</i>, ';
 		if(item.address)
-			$('#conf').append(item.address+', ');
-		$('#conf').append(item.date+', pp. '+item.pages+'.</li>');
+			code += item.address+', ';
+		code += item.date+', pp. '+item.pages+'.</li>';
 	}
-	$('#conf').append('</ul>');
+	code += '</ul>';
+	$('#conf').html(code);
 });
 
