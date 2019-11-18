25729d9857414e1606ec713f57a540176ddf1fed
Hoover H.F. Yin <hooveryin@live.hk>
2019-11-18 23:34:31 +0800

    DOI test

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 $('#journalModal').on('show.bs.modal', function(event) {
 	var cite = $(event.relatedTarget).data('cite');
-	var item = eval("journal_json."+cite);
+	var item = journal_json[cite];
 	$(this).find('.modal-body #journalTitle').html('<a href="'+item.url+'">'+item.title+'</a>');
 	$(this).find('.modal-body #journalAuthor').html(item.fullauthor);
 	$(this).find('.modal-body #journalName').text(item.journal);
@@ -8,28 +8,28 @@ $('#journalModal').on('show.bs.modal', function(event) {
 	$(this).find('.modal-body #journalNumber').text(item.number);
 	$(this).find('.modal-body #journalDate').text(item.date);
 	$(this).find('.modal-body #journalPages').text(item.pages);
-	$(this).find('.modal-boby #journalDoi').text(item.doi);
+	$(this).find('.modal-boby #journalDoi').html(item.doi);
 	$(this).find('.modal-body #journalAbstract').html(item.abstract);
 	$(this).modal('handleUpdate');
 });
 
 $('#confModal').on('show.bs.modal', function(event) {
 	var cite = $(event.relatedTarget).data('cite');
-	var item = eval("conf_json."+cite);
+	var item = conf_json[cite];
 	$(this).find('.modal-body #confTitle').html('<a href="'+item.url+'">'+item.title+'</a>');
 	$(this).find('.modal-body #confAuthor').html(item.fullauthor);
 	$(this).find('.modal-body #confName').text(item.booktitle);
 	$(this).find('.modal-body #confAddress').text(item.address);
 	$(this).find('.modal-body #confDate').text(item.date);
 	$(this).find('.modal-body #confPages').text(item.pages);
-	$(this).find('.modal-boby #confDoi').text(item.doi);
+	$(this).find('.modal-boby #confDoi').html(item.doi);
 	$(this).find('.modal-body #confAbstract').html(item.abstract);
 	$(this).modal('handleUpdate');
 });
 
 $('#preprintModal').on('show.bs.modal', function(event) {
 	var cite = $(event.relatedTarget).data('cite');
-	var item = eval("preprint_json."+cite);
+	var item = preprint_json[cite];
 	$(this).find('.modal-body #preprintTitle').html('<a href="'+item.url+'">'+item.title+'</a>');
 	$(this).find('.modal-body #preprintAuthor').html(item.fullauthor);
 	$(this).find('.modal-body #preprintName').text(item.journal);
@@ -40,7 +40,7 @@ $('#preprintModal').on('show.bs.modal', function(event) {
 
 $('#thesisModal').on('show.bs.modal', function(event) {
 	var cite = $(event.relatedTarget).data('cite');
-	var item = eval("thesis_json."+cite);
+	var item = thesis_json[cite];
 	$(this).find('.modal-body #thesisTitle').html('<a href="'+item.url+'">'+item.title+'</a>');
 	$(this).find('.modal-body #thesisAuthor').html(item.fullauthor);
 	$(this).find('.modal-body #thesisType').text(item.type);
