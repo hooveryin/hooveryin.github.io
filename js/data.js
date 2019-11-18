18398d7a9ac80006c7b8becd816945323467716f
Hoover H.F. Yin <hooveryin@live.hk>
2019-11-18 21:02:23 +0800

    fill in journal list

--- js/data.js
+++ js/data.js
@@ -1,5 +1,23 @@
+var journal_json = {};
+$.getJSON("js/journal.json?nocache="+(new Date()).getTime(), function(json) {
+	journal_json = json;
+	var code = '<ul class="list-group" id="list">';
+	for(cite in json){
+		var item = eval("json."+cite);
+		code += '<li class="list-group-item">'+item.author+', "';
+		if(item.url)
+			code += '<a href="'+item.url+'">';
+		code += item.title;
+		if(item.url)
+			code += '</a>';
+		code += '," <i>'+item.booktitle+'</i>, vol. '+item.volume+', no. '+item.number+', '+item.date+', pp. '+item.pages+'. [<a href="#" data-toggle="modal" data-target="#journalModal" data-cite="'+cite+'">details</a>]</li>';
+	}
+	code += '</ul>';
+	$('#journal').html(code);
+});
+
 var conf_json = {};
-$.getJSON("js/conf.json", function(json) {
+$.getJSON("js/conf.json?nocache="+(new Date()).getTime(), function(json) {
 	conf_json = json;
 	var code = '<ul class="list-group" id="list">';
 	for(cite in json){
@@ -10,10 +28,7 @@ $.getJSON("js/conf.json", function(json) {
 		code += item.title;
 		if(item.url)
 			code += '</a>';
-		code += '," <i>'+item.booktitle+'</i>, ';
-		if(item.address)
-			code += item.address+', ';
-		code += item.date+', pp. '+item.pages+'. [<a href="#" data-toggle="modal" data-target="#confModal" data-cite="'+cite+'">details</a>]</li>';
+		code += '," <i>'+item.booktitle+'</i>, '+item.address+', '+item.date+', pp. '+item.pages+'. [<a href="#" data-toggle="modal" data-target="#confModal" data-cite="'+cite+'">details</a>]</li>';
 	}
 	code += '</ul>';
 	$('#conf').html(code);
--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,18 @@
+$('#journalModal').on('show.bs.modal', function(event) {
+	var cite = $(event.relatedTarget).data('cite');
+	var item = eval("journal_json."+cite);
+	$(this).find('.modal-body #journalTitle').html('<a href="'+item.url+'">'+item.title+'</a>');
+	$(this).find('.modal-body #journalAuthor').html(item.fullauthor);
+	$(this).find('.modal-body #journalName').html(item.journal);
+	$(this).find('.modal-body #journalVolume').html(item.volume);
+	$(this).find('.modal-body #journalNumber').html(item.number);
+	$(this).find('.modal-body #journalDate').html(item.date);
+	$(this).find('.modal-body #journalPages').html(item.pages);
+	$(this).find('.modal-boby #journalDOI').html(item.doi);
+	$(this).find('.modal-body #journalAbstract').html(item.abstract);
+	$(this).modal('handleUpdate');
+});
+
 $('#confModal').on('show.bs.modal', function(event) {
 	var cite = $(event.relatedTarget).data('cite');
 	var item = eval("conf_json."+cite);
