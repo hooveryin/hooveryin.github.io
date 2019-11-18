a051204cb740be22e208f593d8fce7aa49ac97d0
Hoover H.F. Yin <hooveryin@live.hk>
2019-11-18 23:48:26 +0800

    DOI test

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@ $('#journalModal').on('show.bs.modal', function(event) {
 	$(this).find('.modal-body #journalDate').text(item.date);
 	$(this).find('.modal-body #journalPages').text(item.pages);
 	$(this).find('.modal-boby #journalDoi').html(item.date);
+	$(this).find('.modal-boby #journalTest').html(item.doi);
 	$(this).find('.modal-body #journalAbstract').html(item.abstract);
 	$(this).modal('handleUpdate');
 });
