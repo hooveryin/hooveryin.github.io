4fbc8b42be05716f88abd7a351a325d8561bb3e2
Hoover H.F. Yin <hooveryin@live.hk>
2019-11-18 19:36:42 +0800

    modal test

--- js/modal.js
+++ js/modal.js
@@ -3,5 +3,11 @@ $('#confModal').on('show.bs.modal', function(event) {
 	var item = eval("conf_json."+cite);
 	$(this).find('.modal-title').text('Conference Paper');
 	$(this).find('.modal-body #confTitle').html('<a href="'+item.url+'">'+item.title+'</a>');
+	$(this).find('.modal-body #confAuthor').html(item.fullauthor);
+	$(this).find('.modal-body #confName').html(item.booktitle);
+	$(this).find('.modal-body #confAddress').html(item.address);
+	$(this).find('.modal-body #confDate').html(item.date);
+	$(this).find('.modal-body #confAbstract').html(item.abstract);
+	$(this).modal('handleUpdate');
 });
 
